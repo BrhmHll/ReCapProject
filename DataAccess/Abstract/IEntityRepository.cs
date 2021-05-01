7a163996a0ca119b5c77d4b8040b94989aa1fd24
BrhmHll <halil0007644@gmail.com>
2021-05-01 16:52:18 +0300

    little change

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-	public interface IEntityRepository <T>
+	public interface IEntityRepository <T> where T: class, IEntity, new()
 	{
 		List<T> GetAll(Expression<Func<T, bool>> filter=null);
 		T Get(Expression<Func<T, bool>> filter);
