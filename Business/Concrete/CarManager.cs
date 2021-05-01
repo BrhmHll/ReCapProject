e5d0172b93b57b141f5c0efb84672096953c8cd0
BrhmHll <halil0007644@gmail.com>
2021-05-01 16:35:11 +0300

    8th day completed.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -16,12 +16,33 @@ namespace Business.Concrete
 		{
 			_cardal = carDal;
 		}
+
+		public void AddNewCar(Car car)
+		{
+			if (car.Description.Length > 10 && car.DailyPrice > 0 && car.ModelYear > 1900)
+			{
+				_cardal.Add(car);
+			}
+			else
+				Console.WriteLine("Bilgiler eklenemedi!");
+		}
+
 		public List<Car> GetAll()
 		{
 			// Business Codes
 			// if statments
 			// authority control
 			return _cardal.GetAll();
+		}
+
+		public List<Car> GetCarsByBrandId(int id)
+		{
+			return _cardal.GetAll(p => p.BrandId==id);
+		}
+
+		public List<Car> GetCarsByColorId(int id)
+		{
+			return _cardal.GetAll(p => p.ColorId == id);
 		}
 	}
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Business.Concrete;
-using DataAccess.Concrete.InMemory;
+using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,7 +10,18 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
-			CarManager carManager = new CarManager(new InMemoryCarDal());
+			CarManager carManager = new CarManager(new EfCarDal());
+
+			Car car1 = new Car();
+			car1.Id = 2;
+			car1.ModelYear = 1021;
+			car1.BrandId = 2;
+			car1.ColorId = 2;
+			car1.DailyPrice = 300;
+			car1.Description = "Super Car";
+
+			carManager.AddNewCar(car1);
+
 			List<Car> cars = carManager.GetAll();
 
 			foreach (var c in cars)
@@ -20,3 +31,4 @@ namespace ConsoleUI
 		}
 	}
 }
+    
